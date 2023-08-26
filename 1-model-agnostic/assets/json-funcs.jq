cb945f4e8fb6c8c89c9793924ed5bd0ced5ff042
Kasra Farmer <kasra.keshavarz@usask.ca>
2023-08-25 20:20:18 -0400

    model-agnostic workflow prototype created and tested

--- 1-model-agnostic/assets/json-funcs.jq
+++ 1-model-agnostic/assets/json-funcs.jq
@@ -1,15 +1,15 @@
 def parse_args($array):
     array |
     to_entries | .[] |
-    if (.key == "_triggers") then
+    if (.key == "_flags") then
         select ((.value | type == "array") and (.value | length != 0)) |
-            .value | map("--" + . + " \\") | join(" \n")
+            .value | map("--" + . + " ") | join(" ") | @text
     else 
         select (.value != "") |
             if (.value | type == "array") then
-                "--" + .key + "=" + (.value | join(",")) + " \\"
+                "--" + .key + "=" + (.value | join(",")) + " "
             else
-                "--" + .key + "=" + (.value) + " \\"
+                "--" + .key + "=" + (.value) + " "
             end
     end
     ;
@@ -23,4 +23,12 @@ def select_independants($array):
   to_entries | .[] | 
   select((.value == -1) or (.value == "-1")) |
   .key
+  ;
+
+def select_dependants($array):
+  array |
+  to_entries |
+  map(select(.value > 0)) |
+  sort_by(.value) |
+  .[].key
   ;
--- 1-model-agnostic/model-agnostic.sh
+++ 1-model-agnostic/model-agnostic.sh
@@ -12,29 +12,29 @@ echo "$(basename $0): Model-agnostic workflow job submission to SLURM" \
 #########################
 # Temporary file creation
 #########################
-# Temporary file message
-echo "$(basename $0): Creating temporary files..."
-# the directory of the script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# the temp directory used, within $DIR
-# omit the -p parameter to create a temporal directory in the default location
-WORK_DIR=`mktemp -d -p "$DIR"`
-
-# check if tmp dir was created
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-  echo "Could not create temp dir"
-  exit 1
-fi
-
-# deletes the temp directory
-function cleanup {      
-  rm -rf "$WORK_DIR"
-  echo "Deleted temp working directory $WORK_DIR"
-}
-
-# register the cleanup function to be called on the EXIT signal
-trap cleanup EXIT
+## Temporary file message
+#echo "$(basename $0): Creating temporary files..."
+## the directory of the script
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#
+## the temp directory used, within $DIR
+## omit the -p parameter to create a temporal directory in the default location
+#WORK_DIR=`mktemp -d -p "$DIR"`
+#
+## check if tmp dir was created
+#if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+#  echo "Could not create temp dir"
+#  exit 1
+#fi
+#
+## deletes the temp directory
+#function cleanup {      
+#  rm -rf "$WORK_DIR"
+#  echo "Deleted temp working directory $WORK_DIR"
+#}
+#
+## register the cleanup function to be called on the EXIT signal
+#trap cleanup EXIT
 
 
 #####################################
@@ -44,8 +44,16 @@ jqScript="$(dirname $0)/assets/json-funcs.jq"
 
 extract () {
   local func=$1
-  local json=$2
-  jq -r "$(cat $jqScript) $func" $json;
+  jq -r "$(cat $jqScript) $func" $conf;
+}
+
+array_to_csv() {
+  # ChatGPT prompt: Give me a bash function to convert a bash array to
+  # comma-delimited strings
+  local array=("$@")     # Get all arguments as an array
+  local IFS=','          # Set the Internal Field Separator to a comma
+  local result="${array[*]}"
+  echo "$result"
 }
 
 # the `model-agnostic.json` file consists of three main sections
@@ -62,18 +70,10 @@ mapfile -t subKeys < <(jq -r ".${mainKeys[0]} | keys[]" ${conf})
 ################################
 # Checking requirements of $conf
 ################################
-# check if $conf is a valid JSON file
-if jq . $conf >/dev/null 2>$1; then
-  :
-else
-  echo "$(basename $0): ERROR! Input JSON file is not valid"
-  exit 1;
-fi
-
 # Check the `length` of each key and if unequal, throw an error
-initCount=$(extract "count(.${mainKeys[0]})" $conf)
+initCount=$(extract "count(.${mainKeys[0]})")
 for key in "${mainKeys[@]}"; do
-  c=$(extract "count(.${$key})" $conf)
+  c=$(extract "count(.${key})")
   if [[ $c -ne $initCount ]]; then
     echo "$(basename $0): ERROR! The arguments provided in main arrays"
       "are not equal"
@@ -83,11 +83,19 @@ done
 
 # Check if the $mainKeys are provided in $conf
 mapfile -t derivedKeys< <(jq -r "keys[]" ${conf})
+
 for key in "${derivedKeys[@]}"; do
-  if [[ -n "${mainKeys[$key]}" ]]; then
-    echo "$(basename $0): ERROR! \`$key\` it not found in $conf"
-    exit 1;
-  fi
+  for mainKey in "${mainKeys[@]}"; do
+    if [[ "${key,,}" == "${mainKey,,}" ]]; then
+      break 2
+    fi
+  done
+
+  # if break does not happen, throw an error
+  echo "$(basename $0): ERROR! \`$key\` is not found in $conf"
+  echo "key is ${key} and mainKey is ${mainKey}"
+  exit 1;
+
 done
 
 
@@ -96,22 +104,86 @@ done
 #####################
 # first execute those with `order` value of `-1` (i.e., independant)
 # Tip: `indeps` is an array containing "independant" processes
-mapfile -t indeps < <(extract "select_independants(.order)" $conf)
+mapfile -t indeps < <(extract "select_independants(.order)")
+
 for sec in "${indeps[@]}"; do
+
   # select the executable and related arguments
-  executable=$(extract ".exec.${sec}" $conf)
+  executable=$(extract ".exec.${sec}")
   # check how many sets of `args` are provided for each `section`
-  iters=$(extract "count(.args.${indep_sec}[])" $conf)
+  iters=$(extract "count(.args.${sec})")
 
   # iterate over the `args` provided in each `section`
   for i in $(seq 1 $iters); do
+    # index
     idx=$(( i - 1 )) # since JSON/jq array indices start from 0
-    eval `jq -r ".exec.${sec}"` `extract "parse_args(.args.${sec}[$i])"` \
-      >/dev/null 2>&1;
-    echo "$(basename $0): Script for ${sec} for the ${i} iteration is"
-      "executed"
+    # argument value
+    arg=$(extract "parse_args(.args.${sec}[$idx])")
+    # executing
+    `$executable $arg` > /dev/null 2>&1;
+    # relevant message
+    echo "$(basename $0): Script for independant :${sec}:#${i} process" \
+    	"is executed"
   done
 done
 
-# submitting dependant jobs
- 
+
+# second execute those with `order` value of greater than 1 (i.e.,
+# dependant) and ordered monotonically
+# Tip: `deps` is in array containing "dependant" processes 
+mapfile -t deps < <(extract "select_dependants(.order)")
+
+# number of iterations needed, with each iterations being the parent
+# process of the next one
+dep_iters="${#deps[@]}"
+
+# make an empty ID array to keep track of submitted SLURM jobs
+ID=()
+
+for iter in $(seq 1 $dep_iters); do
+  # index in Bash arrays start from 0 
+  idx=$(( $iter - 1 ))
+  # make the $sec variable based on $idx and $deps
+  sec="${deps[$idx]}"
+
+  # select executable and run the process
+  executable=$(extract ".exec.${sec}")
+  
+  # check number of iterations required for each `$deps` process
+  sub_iters=$(extract "count(.args.${sec})")
+
+  for sub_iter in $(seq 1 $sub_iters); do
+    # jq array index starts from 0
+    sub_idx=$(( $sub_iter - 1 ))
+
+    # argument
+    arg=$(extract "parse_args(.args.${sec}[$sub_idx])")
+
+    # executation part 
+    # if parent process
+    if [[ $idx -eq 0 ]]; then
+      # argument value
+      # save SLURM submission ID(s)
+      ID+=$($executable $arg)
+
+      # print parent message
+      echo "$(basename $0): Script for :${sec}:#${sub_iter} process" \
+    	  "is executed for the parent process"
+
+    # if child process
+    else
+      
+      # make comma delimited values of SLURM batch IDs
+      csvID=$(array_to_csv ${ID[@]})
+      
+      # submit child jobs dependant on the parent
+      $executable $arg --dependency=$csvID > /dev/null 2>&1
+
+      # print child message
+      echo "$(basename $0): Script for :${sec}: for the #${sub_iter}" \
+        "process is executed for the child process with parent ID(s) of" \
+        "${ID[@]}"
+    fi
+
+  done
+done
